066285069289e27d6ec343d77b44cb974a3ae693
Gersonpelaes <gersonpelaes@gmail.com>
2025-07-28 23:13:23 -0300

    Versão final do dashboard com base de dados

---
+++ netlify/functions/firebase-config.js
@@ -0,0 +1,9 @@
+{
+  apiKey: "AIzaSyAxuTB2KsuxDi2ww8HEcqqi_tnzzPVR8Lw",
+  authDomain: "cmv-ia.firebaseapp.com",
+  projectId: "cmv-ia",
+  storageBucket: "cmv-ia.firebasestorage.app",
+  messagingSenderId: "587984899697",
+  appId: "1:587984899697:web:d268195678b0e81cb515d0",
+  measurementId: "G-V0R45F3XS5"
+};
