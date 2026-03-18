e3040770015bd97f04d3ad301a16bfbdeeb6754f
Gersonpelaes <gersonpelaes@gmail.com>
2026-03-18 15:29:41 -0300

    security: Oculta a API Key do Firebase movendo pra env vars da Netlify e usa gemini-proxy

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -7,13 +7,13 @@ exports.handler = async function(event, context) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      apiKey: process.env.VITE_"AIzaSyAxuTB2KsuxDi2ww8HEcqqi_tnzzPVR8Lw",
-      authDomain: process.env.VITE_"cmv-ia.firebaseapp.com",
-      projectId: process.env.VITE_"cmv-ia",
-      storageBucket: process.env.VITE_"cmv-ia.firebasestorage.app",
-      messagingSenderId: process.env.VITE_"587984899697",
-      appId: process.env.VITE_"1:587984899697:web:d268195678b0e81cb515d0",
-      measurementId: process.env.VITE_"G-V0R45F3XS5"
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     }),
   };
 };
